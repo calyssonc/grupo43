bd6f406b4335e5e323357e8d804fd3293ef387eb
Carlos Eduardo <carlos.edumart@outlook.com>
2021-03-24 21:44:08 -0300

    Fix-Error Escola

--- app/Models/escola.php
+++ app/Models/escola.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Escola extends Model
+class escola extends Model
 {
     use HasFactory;
 
