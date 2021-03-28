e554528cc5183a907a9a385a928ddb0db55b64c7
Cicero Alysson <calyssonc@gmail.com>
2021-03-28 17:55:53 -0300

    ajuste de cor do template
    deixei mais claro para ficar melhor de ver os detalhes

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') - {{ config('app.name') }} </title>
-    <link rel="stylesheet" href="https://bootswatch.com/4/darkly/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
