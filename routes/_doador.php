d06587e9325906bc0aa141f6b8e96c2e9ce59d5e
Carlos Eduardo <carlos.edumart@outlook.com>
2021-03-29 21:31:19 -0300

    Postman funcionando

--- routes/_doador.php
+++ routes/_doador.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\DoadorController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::group(['middleware' => ['auth', 'role:doador']], function () {
     Route::get('/dashboard-doador', [DashboardController::class, 'index'])->name('dashboard');
