ad0ab86f44c576d0f9d7dfcc6a114498b69e2924
Cicero Alysson <calyssonc@gmail.com>
2021-04-01 22:00:24 -0300

    ajustes index
    nomes do footer agora estão corretos e icones de mídias sociais estão na horizontal

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -25,13 +25,13 @@
         </div>
         <div class="h-20 w-full sm:w-2/6 flex flex-row justify-center items-center text-center">
             <div class="p-2 w-2/6">
-                <p>Mobile app</p>
+                <p>Ajuda</p>
             </div>
             <div class="p-2 w-2/6">
-                <p>Comunidade</p>
+                <p>Blog</p>
             </div>
             <div class="p-2 w-2/6">
-                <p>Quem somos</p>
+                <p>Parceiros</p>
             </div>
         </div>
     </div>
@@ -41,12 +41,12 @@
 </div>
 <div class="h-44 w-full">
     <div class="flex flex-wrap justify-center">
-        <div class="w-full sm:w-auto p-3 flex justify-center items-center">
+        <div class="w-auto p-3 flex justify-center items-center">
             <a href="#">
                 <svg width="52" height="56" enable-background="new 0 0 32 32" id="Layer_1" version="1.0" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><circle clip-rule="evenodd" cx="16" cy="16" fill="#3B5998" fill-rule="evenodd" r="16"/><path d="M22,32V20h4l1-5h-5v-2c0-2,1.002-3,3-3h2V5c-1,0-2.24,0-4,0c-3.675,0-6,2.881-6,7v3h-4v5h4v12H22   z" fill="#FFFFFF" id="f_2_"/></g><g/><g/><g/><g/><g/><g/></svg>
             </a>
         </div>
-        <div class="w-full sm:w-auto p-3 flex justify-center items-center">
+        <div class="w-auto p-3 flex justify-center items-center">
             <a href="#">
                 <svg id="Layer_1" width="52" height="56" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><style type="text/css">
                     .st0{fill:#25D366;}
@@ -78,7 +78,7 @@
                 </style><g><linearGradient gradientUnits="userSpaceOnUse" id="SVGID_1_" x1="407.1325" x2="104.8675" y1="4.4733" y2="507.5267"><stop offset="0" style="stop-color:#4845A2"/><stop offset="4.536461e-02" style="stop-color:#5945A2"/><stop offset="0.1713" style="stop-color:#8444A1"/><stop offset="0.2698" style="stop-color:#9E44A1"/><stop offset="0.3282" style="stop-color:#A844A1"/><stop offset="0.3905" style="stop-color:#AB429A"/><stop offset="0.4788" style="stop-color:#B43C88"/><stop offset="0.4896" style="stop-color:#B53B85"/><stop offset="0.5577" style="stop-color:#BE3572"/><stop offset="0.6762" style="stop-color:#D22749"/><stop offset="0.7388" style="stop-color:#DF4F3E"/><stop offset="0.8559" style="stop-color:#F9A326"/><stop offset="1" style="stop-color:#F9DD26"/></linearGradient><path class="st4" d="M391.5,13H120.5C61.1,13,13,61.1,13,120.5v270.9C13,450.9,61.1,499,120.5,499h270.9   c59.4,0,107.5-48.1,107.5-107.5V120.5C499,61.1,450.9,13,391.5,13z"/><circle class="st1" cx="352.4" cy="160" r="21.2"/><g><path class="st1" d="M333.8,76H178.2C121.8,76,76,121.8,76,178.2v155.6C76,390.2,121.8,436,178.2,436h155.6    c56.4,0,102.2-45.9,102.2-102.2V178.2C436,121.8,390.2,76,333.8,76z M403.5,333.8c0,38.4-31.3,69.7-69.7,69.7H178.2    c-38.4,0-69.7-31.3-69.7-69.7V178.2c0-38.4,31.3-69.7,69.7-69.7h155.6c38.4,0,69.7,31.3,69.7,69.7V333.8z"/><path class="st1" d="M256,162.3c-51.7,0-93.7,42-93.7,93.7s42,93.7,93.7,93.7s93.7-42,93.7-93.7S307.7,162.3,256,162.3z     M256,317.2c-33.7,0-61.2-27.5-61.2-61.2s27.5-61.2,61.2-61.2s61.2,27.5,61.2,61.2S289.7,317.2,256,317.2z"/></g></g></svg>
             </a>
         </div>
-        <div class="w-full sm:w-auto p-3 flex justify-center items-center">
+        <div class="w-auto p-3 flex justify-center items-center">
             <a href="#">
                 <svg id="Layer_1" width="52" height="56" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><style type="text/css">
                     .st0{fill:#25D366;}
@@ -110,7 +110,7 @@
                 </style><g><path class="st0" d="M256,13C121.8,13,13,121.8,13,256c0,49.1,14.6,94.9,39.7,133.1L21.4,495.1l110.1-30.5   c36.4,21.8,79,34.3,124.5,34.3c134.2,0,243-108.8,243-243C499,121.8,390.2,13,256,13z"/><path class="st1" d="M389.5,322.7c-0.3-3.2-2.2-6-5.1-7.3c-19.6-9-39.3-18-58.9-27c-2.2-1-4.9-0.5-6.5,1.4l-26.6,30   c-2.1,2.4-5.4,3.2-8.3,2c-13.8-5.7-32.4-15.3-51.2-31.5c-22.5-19.3-35.7-40-43.1-53.5c3.7-2.5,13.8-10,18.3-23.9   c0-0.1,0.1-0.2,0.1-0.2c2.8-8.6,2.2-17.9-0.9-26.4c-5.3-14.1-15.9-41.4-20-45.6c-0.7-0.7-1.5-1.4-1.5-1.4c-3-2.7-6.9-4.2-11-4.4   c-1.4-0.1-2.9-0.1-4.5-0.1c-4.6-0.1-8.7-0.1-11.7,0.3c-8.5,1.2-14.7,6.9-18.6,11.7c-4.7,5.7-10.3,14.2-14,25.4   c-0.3,1-0.6,1.9-0.9,2.9c-4.1,14.2-3.3,29.4,1.6,43.4c4.6,13.1,11.3,28.7,21.4,45.2c17.1,28.1,35.7,45.5,48.3,57   c14.2,13.1,31.8,29.2,59.7,42.6c25.8,12.4,49.2,17,64.9,18.9c6.4,0.6,18.2,0.7,31.7-4.4c6.2-2.4,11.4-5.3,15.6-8.4   c11.1-7.9,18.9-19.8,20.9-33.2c0-0.1,0-0.2,0-0.3C389.9,330.8,389.8,326.4,389.5,322.7z"/></g></svg>
             </a>
         </div>
-        <div class="w-full sm:w-auto p-3 flex justify-center items-center">
+        <div class="w-auto p-3 flex justify-center items-center">
             <a href="#">
                 <svg width="52" height="56" enable-background="new 0 0 48 48" id="Layer_1" version="1.1" viewBox="0 0 48 48" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><circle cx="24" cy="24" fill="#1CB7EB" r="24"/><g><g><path d="M36.8,15.4c-0.9,0.5-2,0.8-3,0.9c1.1-0.7,1.9-1.8,2.3-3.1c-1,0.6-2.1,1.1-3.4,1.4c-1-1.1-2.3-1.8-3.8-1.8    c-2.9,0-5.3,2.5-5.3,5.7c0,0.4,0,0.9,0.1,1.3c-4.4-0.2-8.3-2.5-10.9-5.9c-0.5,0.8-0.7,1.8-0.7,2.9c0,2,0.9,3.7,2.3,4.7    c-0.9,0-1.7-0.3-2.4-0.7c0,0,0,0.1,0,0.1c0,2.7,1.8,5,4.2,5.6c-0.4,0.1-0.9,0.2-1.4,0.2c-0.3,0-0.7,0-1-0.1    c0.7,2.3,2.6,3.9,4.9,3.9c-1.8,1.5-4.1,2.4-6.5,2.4c-0.4,0-0.8,0-1.3-0.1c2.3,1.6,5.1,2.6,8.1,2.6c9.7,0,15-8.6,15-16.1    c0-0.2,0-0.5,0-0.7C35.2,17.6,36.1,16.6,36.8,15.4z" fill="#FFFFFF"/></g></g></svg>
             </a>
