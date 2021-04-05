e86a6cd344e961be9f9ae3eb1cdcdb85948e0a3f
Carlos Eduardo <carlos.edumart@outlook.com>
2021-04-05 02:30:19 -0300

    Dashboardo Corrigido

--- resources/views/escola/index.blade.php
+++ resources/views/escola/index.blade.php
@@ -1,103 +1,128 @@
 @extends('template/template')
 
-@section('title','Escola Index')
+@section('title','Perfil escola')
 
 @section('content')
 
 
-
-<div class="p-11 text-center text-3xl lg:text-6xl">
-    <h1> Pesquise por escolas próximas a você! </h1>
+<div class="grid  grid-rows-2 mx-2 mt-2  p-4 bg-gradient-to-br from-blue-400 to-blue-200 rounded-t-3xl lg:grid-cols-3 lg:grid-rows-1">
+    <div class="row-span-1 mx-2 place-self-center lg:col-span-1">
+        <div class="border-2 border-black-500 rounded-full w-48 w-48 overflow-hidden">
+            <img class="w-42 h-42" src="https://i.pinimg.com/originals/99/8e/1c/998e1cce79d61f1f3d5c69091a5bd455.jpg">
+        </div>
+    </div>
+    <div class="row-span-1 mx-2 lg:col-span-2">
+        <h1 class="font-bold text-2xl text-white">{{$escola->name}}</h1>
+        <div class="py-3">
+            <h1 class="text-bold mb-1">Lv: 2</h1>
+            <div class="mb-2 h-4 relative max-w-xl rounded-full overflow-hidden">
+                <div class=" w-full h-full bg-gray-200 absolute"></div>
+                <div class="h-full bg-yellow-500 absolute" style="width:30%"></div>
+            </div>
+            <p class="uppercase"> Escola {{$escola->tipo}}</p>
+            <p>{{$escola->localizacao}}</p>
+            <h3>Contato :</h3>
+            <ul class="list-inside bg-rose-200 ...">
+                <li>
+                    <p>{{$escola->telefone}}</p>
+                </li>
+                <li>
+                    <p>{{$escola->email}}</p>
+                </li>
+            </ul>
+        </div>
+    </div>
 </div>
-
-<div class="grid grid-cols-1 w-screen
-            lg:grid-cols-2 pr-3 w-full">
-    <div class="p-2 lg:p-8
-                lg:min-h-screen lg:items-center">
-        <form action="{{ route('escola.search') }}" method="post" class="grid-rows-1 border rounded-xl my-4 bg-gradient-to-br from-gray-100">
-            @csrf
-            <div class="flex flex-row py-4 px-1 ">
-                <button type="submit" class="w-14 rounded-full shadow-sm border-2 hover:border-blue-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                </button>
-                <div class="mt-1 flex rounded-full shadow-sm w-full">
-                    <input type="text" name="search" id="search" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-full sm:text-sm border-gray-300" placeholder="Digite o nome da escola">
-                </div>
-            </div>
-
-            <div class="flex flex-row ...">
-                <select id="estado" name="estado" class="m-2 mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                    <option value="" disabled selected>Estado</option>
-                    <option>São Paulo</option>
-                    <option>Ceara</option>
-                    <option>Santa Catarina</option>
-                </select>
-                <select id="cidade" name="cidade" class="m-2 mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                    <option value="" disabled selected>Cidade</option>
-                    <option>São Paulo</option>
-                    <option>Mongaguá</option>
-                    <option>Santos</option>
-                    <option>São Vicente</option>
-                    <option>CiceroCity</option>
-                    <option>DafneCity</option>
-                </select>
-                <select id="bairro" name="bairro" class="m-2 mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                    <option value="" disabled selected>Bairro</option>
-                    <option>Aguápeu</option>
-                    <option>Parque Continental</option>
-                    <option>Aparecida</option>
-                </select>
-            </div>
-        </form>
-
-
-
-        <div class="border-4"></div>
-        @empty($escolas)
-        Não há escolas cadastradas
-        @endempty
-        @foreach ($escolas as $escola)
-        <div class="
-            h-32 bg-gradient-to-br from-gray-200 shadow-2xl border-2 border-gray-300 hover:border-blue-500 m-2 rounded-3xl p-2 grid grid-cols-5 lg:rounded-full lg:min-h-42 ">
-            <div class="text-center col-span-1 ">
-                <div class="py-3">
-                    <div class="w-3/5 h-3/5 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-full text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10 lg-w-3/4">
-
-                    </div>
-                </div>
-            </div>
-            <div class="text-center grid grid-cols-1 col-span-3 m-2  ">
-                <div class="p-2">
-                    <h1 class=" font-bold text-sm uppercase"> {{ $escola->name }} </h1>
-                </div>
-                <div class="text-sm">
-                    <p class="uppercase">{{ $escola->localizacao}}</p>
-                </div>
-                <div class="text-sm">
-                    <p>{{ $escola->telefone }}</p>
-                </div>
-            </div>
-            <div class="flex col-span-1">
-                <div class="py-4 w-full">
-                    <a href="{{ route('escola.show',$escola->id)}}" class="w-10/12 text-xs flex items-center justify-center px-7 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 md:py-4 md:text-lg md:px-10 lg-w-3/4 lg:text-lg ">
-                        Doar!
-                    </a>
-                </div>
+<div class="grid grid-row-2 bg-gradient-to-tr from-blue-400 to-blue-200 mx-2 rounded-b-3xl lg:bg:white lg:grid-cols-3 lg:grid-row-1 ">
+    <div class="grid grid-cols-6 gap-2 lg:col-start-2 lg:col-span-2">
+        <div class="place-self-center ...">
+            <div class="bg-white rounded-full mt-2 h-10 md:h-20 w-10 md:w-20 flex justify-center items-center">
+                <p>+</p>
             </div>
         </div>
-        @endforeach
+        <div class="place-self-center ...">
+            <div class="bg-white rounded-full mt-2 h-10 md:h-20 w-10 md:w-20 flex justify-center items-center">
+                <p>+</p>
+            </div>
+        </div>
+        <div class="place-self-center ...">
+            <div class="bg-white rounded-full mt-2 h-10 md:h-20 w-10 md:w-20 flex justify-center items-center">
+                <p>+</p>
+            </div>
+        </div>
+        <div class="place-self-center ...">
+            <div class="bg-white rounded-full mt-2 h-10 md:h-20 w-10 md:w-20 flex justify-center items-center">
+                <p>+</p>
+            </div>
+        </div>
+        <div class="place-self-center ...">
+            <div class="bg-white rounded-full mt-2 h-10 md:h-20 w-10 md:w-20 flex justify-center items-center">
+                <p>+</p>
+            </div>
+        </div>
+        <div class="place-self-center ...">
+            <div class="bg-white rounded-full mt-2 h-10 md:h-20 w-10 md:w-20 flex justify-center items-center">
+                <p>+</p>
+            </div>
+        </div>
     </div>
-    <div class="p-2 h-0">
-        <img class="hidden md:hidden lg:block" src="{{ asset('image/booklover.png') }}">
+
+    <div class="grid grid-flow-row grid-rows-3 w-full p-4 lg:row-start-1 ">
+        <div class="row-span-1 mx-2 p-2">
+            <button class="shadow-2xl rounded-xl p-2 w-full bg-gradient-to-br from-blue-500 to-blue-700 text-white lg:w-1/2">Medalhas</button>
+        </div>
+        <div class="row-span-1 mx-2 p-2">
+            <button class="shadow-2xl rounded-xl p-2 w-full bg-gradient-to-br from-blue-500 to-blue-700 text-white lg:w-1/2">Galeria</button>
+        </div>
+        <div class="row-span-1 mx-2 p-2">
+            <button class="shadow-2xl rounded-xl p-2 w-full bg-gradient-to-br from-blue-500 to-blue-700 text-white lg:w-1/2">Saiba mais</button>
+        </div>
+        <div class="row-span-1 mx-2 p-2">
+            <a class="shadow-2xl rounded-xl p-2 w-full bg-gradient-to-br from-blue-500 to-blue-700 text-white lg:w-1/2 text-center" href="{{ route('logout') }}">Sair</a>
+        </div>
     </div>
+</div>
 
+<div class="grid grid-cols-3 my-2 py-10 border-gray-300 border-2 rounded-3xl m-2 shadow-sm bg-gray-200">
+    <div class="place-self-center">
+        <img class="w-24 h-24 border-2 border-black-500 rounded-full" src="https://cdn.discordapp.com/attachments/822990528931954688/828110878892359700/medalha_1.png">
+    </div>
+    <div class="grid grid-cols-2 col-span-2 p-2 lg:grid-rows-1 lg:grid-cols-4">
+        <div class="text-center p-2">
+            <p class="font-bold">Posição no ranking</p>
+            <p class="text-3xl font-bold lg:text-6xl">50</p>
+        </div>
+        <div class="text-center p-2">
+            <p class="font-bold">Famílias Cadastradas</p>
+            <p class="text-3xl font-bold lg:text-6xl">250</p>
+        </div>
+        <div class="text-center p-2">
+            <p class="font-bold">Famílias beneficiadas</p>
+            <p class="text-3xl font-bold lg:text-6xl">170</p>
+        </div>
+        <div class="text-center p-2">
+            <p class="font-bold">Doações recebidas</p>
+            <p class="text-3xl font-bold lg:text-6xl">150</p>
+        </div>
+    </div>
+</div>
 
-    <style>
-        h2 {
-            margin-top: 35px;
-        }
-    </style>
-
-    @endsection
+<div class="grid grid-cols-1 w-screen lg:grid-cols-2 min-h-min">
+    <div class="border-2 m-2 rounded-3xl border-black text-center bg-gradient-to-tr from-blue-400 to-blue-200">
+        <h1 class="text-2xl font-bold py-1">Lista de alunos</h1>
+        <div class="border-2 m-2 rounded-3xl border-black bg-white">
+            <div class="grid grid-cols-3 border-black border-2 m-2 rounded-3xl bg-white shadow-sm">
+                <h1 class="text-2xl font-bold col-span-2 self-center"> Juquinha </h1>
+                <button class="border-2 m-1 rounded-2xl bg-blue-400 text-white">Ver Lista Completa</button>
+            </div>
+            <div class="grid grid-cols-3 border-black border-2 m-2 rounded-3xl bg-white shadow-sm">
+                <h1 class="text-2xl font-bold col-span-2 self-center"> Maria </h1>
+                <button class="border-2 m-1 rounded-2xl bg-blue-400 text-white">Ver Lista Completa</button>
+            </div>
+        </div>
+    </div>
+    <div class="p-2">
+        <img class="hidden md:hidden  lg:block" src="{{ asset('image/booklover.png') }}">
+    </div>
+</div>
+@endsection
